7cbfeedd62cb01b2803224908023e6792ede48e9
saimaideepyadav <31813835+saimaideepyadav@users.noreply.github.com>
2021-04-30 02:28:21 +0530

    server action

--- server.sh
+++ server.sh
@@ -1,2 +1,10 @@
-cd app/ && rasa run --model /app/models --enable-api --cors "*" --debug -p $PORT
-cd /app && rasa run actions -p $PORT
+cd app/
+# Start rasa server with nlu model
+rasa run --model models --enable-api --cors "*" --debug \
+          -p $PORT
+		  
+		  
+cd app/
+# Start rasa server with nlu model
+rasa run actions --cors "*" --debug \
+          -p $PORT
